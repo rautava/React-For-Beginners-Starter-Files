314419ef4ace655ba0650bd7fc10c13823a316ea
Tommi Rautava <tommi.rautava@ge.com>
2019-02-14 18:13:29 +0200

    12

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-  myInput = React.createRef();
+  constructor(props) {
+    super(props);
+    this.myInput = React.createRef();
+  }
 
   goToStore = event => {
     event.preventDefault();
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
   };
 
   render() {
@@ -14,6 +19,7 @@ class StorePicker extends React.Component {
         <h2>Please Enter A Store</h2>
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
