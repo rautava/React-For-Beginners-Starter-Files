380428042459ac9c415d7c74840c892dcdf3a722
Tommi Rautava <tommi.rautava@ge.com>
2019-02-14 17:40:47 +0200

    10

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 
-const StorePicker = props => {
-  return (
-    <form className="store-selector">
-      <h2>Please Enter A Store</h2>
-      <input type="text" required placeholder="Store Name" />
-      <button type="submit">Visit Store →</button>
-    </form>
-  );
-};
+class StorePicker extends Component {
+  render() {
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store →</button>
+      </form>
+    );
+  }
+}
 
 export default StorePicker;
